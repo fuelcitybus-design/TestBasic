9fa6127ec7ca2e5d23427ac89560559227617b6c
fuelcitybus-design <fuelcitybus@gmail.com>
2026-05-26 15:17:37 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello from Azure App Service!"
+
+@app.route("/health")
+def health():
+    return "OK"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Azure expects 8000
+    app.run(host="0.0.0.0", port=port)

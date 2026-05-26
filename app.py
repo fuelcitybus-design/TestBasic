f2f128c673f9319720023b7e1708337913704b4d
fuelcitybus-design <fuelcitybus@gmail.com>
2026-05-26 15:30:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,3 @@ demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 
 # Expose Gradio app for Gunicorn
 app = demo.app
-
-if __name__ == "__main__":
-    # Local testing only
-    demo.launch(server_name="0.0.0.0", server_port=8000)
